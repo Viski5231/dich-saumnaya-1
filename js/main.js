574038e1284c9c8b9f9452d5d206ac7506a38799
Viski5231 <artemagodin135@gmail.com>
2025-02-03 11:02:39 +0700

    dich saumnaya 6

--- js/main.js
+++ js/main.js
@@ -10,7 +10,20 @@ let app = new Vue({
         inStock: true,
         inventory: 100,
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        onSale: true
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+
     }
 })
 
